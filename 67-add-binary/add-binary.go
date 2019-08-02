8c9d012ea7bbc1438c0a8f10788119d097665150
JokerYellow <piase1910@163.com>
2019-08-02 08:13:04 +0800

    [add]:add binary;

--- 67-add-binary/add-binary.go
+++ 67-add-binary/add-binary.go
@@ -26,8 +26,48 @@ Example 2:
 Input: a = "1010", b = "1011"
 Output: "10101"
 */
-//TODO:complete it
-func addBinary(a string, b string) string {
 
-	return ""
+func addBinary(a string, b string) string {
+	i := len(a) - 1
+	j := len(b) - 1
+	cv := 0
+	var bytes []byte
+
+	for i >= 0 || j >= 0 || cv > 0 {
+		vi := 0
+		if i >= 0 {
+			if a[i] == '1' {
+				vi = 1
+			} else {
+				vi = 0
+			}
+		}
+		vj := 0
+		if j >= 0 {
+			if b[j] == '1' {
+				vj = 1
+			} else {
+				vj = 0
+			}
+		}
+		sum := vi + vj + cv
+		if sum%2 == 1 {
+			bytes = append(bytes, '1')
+		} else {
+			bytes = append(bytes, '0')
+		}
+		cv = sum / 2
+		i--
+		j--
+	}
+	return string(reverseBytes(bytes))
+}
+
+func reverseBytes(origin []byte) []byte {
+	length := len(origin)
+	rt := make([]byte, len(origin))
+	for i, v := range origin {
+		rt[length-i-1] = v
+	}
+	return rt
 }
--- 67-add-binary/add-binary_test.go
+++ 67-add-binary/add-binary_test.go
@@ -1,12 +1,39 @@
 package _7_add_binary
 
-import "testing"
+import (
+	"github.com/jokerYellow/leetcode/utils"
+	"testing"
+)
 
 func Test1(t *testing.T) {
 	a := "11"
 	b := "1"
 	expect := "100"
-	if expect != addBinary(a, b) {
+	output := addBinary(a, b)
+	if expect != output {
+		utils.Print(expect, output)
+		t.Fail()
+	}
+}
+
+func Test2(t *testing.T) {
+	a := "100001"
+	b := "1"
+	expect := "100010"
+	output := addBinary(a, b)
+	if expect != output {
+		utils.Print(expect, output)
+		t.Fail()
+	}
+}
+
+func Test3(t *testing.T) {
+	a := "1"
+	b := "0"
+	expect := "1"
+	output := addBinary(a, b)
+	if expect != output {
+		utils.Print(expect, output)
 		t.Fail()
 	}
 }
