8808b1f30c2bd7df39e897ea6702d9f38f3be862
JokerYellow <piase1910@163.com>
2019-11-10 15:42:22 +0800

    [update]:faster 3sum

--- 15-3sum/3sum.go
+++ 15-3sum/3sum.go
@@ -9,16 +9,20 @@ func threeSum(nums []int) [][]int {
 		if i > 0 && nums[i] == nums[i-1] {
 			continue
 		}
-		for j := i + 1; j < len(nums)-1; j++ {
-			opposite := - nums[i] - nums[j]
-			if j > i + 1 && nums[j] == nums[j-1]{
-				continue
-			}
-			for k := j + 1; k < len(nums); k++ {
-				if nums[k] == opposite {
-					components = append(components, []int{nums[i], nums[j], nums[k]})
-					break
+		l := i + 1
+		r := len(nums) - 1
+		for l < r {
+			sum := nums[i] + nums[l] + nums[r]
+			if sum == 0 {
+				components = append(components, []int{nums[i], nums[l], nums[r]})
+				for l < len(nums)-1 && nums[l] == nums[l+1] {
+					l++
 				}
+				l++
+			} else if sum > 0 {
+				r--
+			} else if sum < 0 {
+				l++
 			}
 		}
 	}
