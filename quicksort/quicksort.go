30cc2fe528747c82ebed406510d60b7e1a316922
JokerYellow <piase1910@163.com>
2019-11-10 15:42:49 +0800

    [add]：clear quick sort

--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -12,19 +12,15 @@ func quisksort(arr []int) []int {
 //increase
 //move the arr,make left of the index is littler ,right is bigger
 func part(arr []int, index, leftBound, rightBound int) {
-	if leftBound >= rightBound {
-		return
-	}
 	left := leftBound
 	right := rightBound
 	for left < right {
 		for left < index {
-			if arr[left] < arr[index] {
+			if arr[left] <= arr[index] {
 				left++
 			} else {
 				arr[left], arr[index] = arr[index], arr[left]
 				index = left
-				left++
 				break
 			}
 		}
@@ -34,11 +30,14 @@ func part(arr []int, index, leftBound, rightBound int) {
 			} else {
 				arr[right], arr[index] = arr[index], arr[right]
 				index = right
-				right--
 				break
 			}
 		}
 	}
-	part(arr, leftBound, leftBound, index-1)
-	part(arr, index+1, index+1, rightBound)
+	if leftBound < index-1 {
+		part(arr, leftBound, leftBound, index-1)
+	}
+	if index+1 < rightBound {
+		part(arr, index+1, index+1, rightBound)
+	}
 }
