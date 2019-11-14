c51b5c1664da0eca4177eae35cac0749821a7604
JokerYellow <piase1910@163.com>
2019-11-14 19:42:53 +0800

    [update]:quick sort

--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -4,40 +4,45 @@ func quisksort(arr []int) []int {
 	if len(arr) == 0 {
 		return arr
 	}
-	out := arr
-	part(out, 0, 0, len(arr)-1)
-	return out
+	quickSort(arr, 0, len(arr)-1)
+	return arr
+}
+
+func quickSort(arr []int, start, end int) {
+	if start >= end {
+		return
+	}
+	index := part(arr, start, start, len(arr)-1)
+	if index-1 > start {
+		quickSort(arr, start, index-1)
+	}
+	if index+1 < end {
+		quickSort(arr, index+1, end)
+	}
 }
 
 //increase
 //move the arr,make left of the index is littler ,right is bigger
-func part(arr []int, index, leftBound, rightBound int) {
+func part(arr []int, index, leftBound, rightBound int) int {
 	left := leftBound
 	right := rightBound
 	for left < right {
-		for left < index {
+		if left < index {
 			if arr[left] <= arr[index] {
 				left++
 			} else {
 				arr[left], arr[index] = arr[index], arr[left]
 				index = left
-				break
 			}
 		}
-		for right > index {
+		if index < right {
 			if arr[right] >= arr[index] {
 				right--
 			} else {
 				arr[right], arr[index] = arr[index], arr[right]
 				index = right
-				break
 			}
 		}
 	}
-	if leftBound < index-1 {
-		part(arr, leftBound, leftBound, index-1)
-	}
-	if index+1 < rightBound {
-		part(arr, index+1, index+1, rightBound)
-	}
+	return index
 }
