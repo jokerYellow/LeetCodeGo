bc03590dab4cee1f837fc7746d8e61264222dfd8
JokerYellow <piase1910@163.com>
2019-11-17 17:07:53 +0800

    [fix]:binary search

--- midSearch/midSearch.go
+++ midSearch/midSearch.go
@@ -1,11 +1,14 @@
 package midSearch
 
 func midSearch(nums []int, value int) int {
+	if len(nums) == 0 {
+		return -1
+	}
 	return midSearchSub(nums, value, 0, len(nums)-1)
 }
 
 func midSearchSub(nums []int, value int, start, end int) int {
-	if start > end {
+	if nums[start] > value || nums[end] < value {
 		return -1
 	}
 	mid := start + (end-start)/2
@@ -13,6 +16,7 @@ func midSearchSub(nums []int, value int, start, end int) int {
 		return midSearchSub(nums, value, start, mid-1)
 	} else if nums[mid] < value {
 		return midSearchSub(nums, value, mid+1, end)
+	} else {
+		return mid
 	}
-	return mid
 }
--- midSearch/midSearch_test.go
+++ midSearch/midSearch_test.go
@@ -1,6 +1,10 @@
 package midSearch
 
-import "testing"
+import (
+	"fmt"
+	"github.com/jokerYellow/leetcode/utils"
+	"testing"
+)
 
 func Test(t *testing.T) {
 	test(t, []int{1}, 2, -1)
@@ -8,10 +12,14 @@ func Test(t *testing.T) {
 	test(t, []int{2}, 2, 0)
 
 	test(t, []int{1, 2, 3, 4}, 2, 1)
+	test(t, []int{1, 2, 3, 4, 5, 9}, 6, -1)
 }
 
 func test(t *testing.T, nums []int, value int, expectIndex int) {
-	if expectIndex != midSearch(nums, value) {
+	output := midSearch(nums, value)
+	if expectIndex != output {
+		fmt.Println(nums, value)
+		utils.Print(expectIndex, output)
 		t.Fail()
 	}
 }
