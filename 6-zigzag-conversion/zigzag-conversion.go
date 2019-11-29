47f84361fe551e25154ac6b629a66b1da473cb25
JokerYellow <piase1910@163.com>
2019-11-29 08:31:15 +0800

    [update]：zigzag-conversion,use buffer instead slice

--- 6-zigzag-conversion/zigzag-conversion.go
+++ 6-zigzag-conversion/zigzag-conversion.go
@@ -9,17 +9,9 @@ func convert(s string, numRows int) string {
 	if numRows == 1 {
 		return s
 	}
-	length := len(s)
-	numLine := (length / (numRows*2 - 2)) * (numRows - 1)
-	more := length % (numRows*2 - 2)
-	if more <= numRows && more > 0 {
-		numLine += 1
-	} else if more > numRows {
-		numLine += more - numRows + 1
-	}
-	arr := make([][]rune, numRows)
+	arr := make([]bytes.Buffer, numRows)
 	for i := 0; i < numRows; i++ {
-		arr[i] = make([]rune, numLine)
+		arr[i] = bytes.Buffer{}
 	}
 	for i, c := range s {
 		offset := i / (numRows*2 - 2)
@@ -32,15 +24,11 @@ func convert(s string, numRows int) string {
 			row = numRows - (more+1)%numRows - 1
 			number += (more + 1) % numRows
 		}
-		arr[row][number] = c
+		arr[row].WriteRune(c)
 	}
 	buffer := bytes.Buffer{}
 	for _, item := range arr {
-		for _, c := range item {
-			if c != 0 {
-				buffer.WriteRune(c)
-			}
-		}
+		buffer.Write(item.Bytes())
 	}
 	return buffer.String()
 }
