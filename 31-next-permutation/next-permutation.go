14f4c5b71df8add1347018c68ed87da5534ae14a
JokerYellow <piase1910@163.com>
2019-12-10 22:48:38 +0800

    [refactor]:next permutation.

--- 31-next-permutation/next-permutation.go
+++ 31-next-permutation/next-permutation.go
@@ -1,44 +1,51 @@
 package leetcode
 
+/*
+https://leetcode.com/problems/next-permutation
+31. Next Permutation
+Medium
+
+2445
+
+815
+
+Favorite
+
+Share
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+
+The replacement must be in-place and use only constant extra memory.
+
+Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
+*/
 func nextPermutation(nums []int) []int {
 	if len(nums) <= 1 {
 		return nums
 	}
-	lastIndex := len(nums) - 1
-	lastValue := nums[lastIndex]
-	for i := len(nums) - 2; i >= 0; i-- {
-		if lastValue > nums[i] {
-			lastValue = nums[i]
-			lastIndex = i
-			break
-		}
-		lastValue = nums[i]
+	lastIndex := len(nums) - 2
+	for lastIndex >= 0 && nums[lastIndex] >= nums[lastIndex+1] {
+		lastIndex--
 	}
-	if lastIndex == len(nums)-1 {
-		i := 0
-		j := len(nums) - 1
-		for i < j {
-			nums[i], nums[j] = nums[j], nums[i]
-			i++
-			j--
-		}
-	} else {
+	if lastIndex >= 0 {
 		minBiggerIndex := lastIndex + 1
-		for i := lastIndex + 2; i < len(nums); i++ {
-			if nums[i] > lastValue {
-				minBiggerIndex = i
-			} else if nums[i] < lastValue {
-				break
-			}
+		for minBiggerIndex < len(nums)-1 && nums[minBiggerIndex+1] > nums[lastIndex] {
+			minBiggerIndex++
 		}
 		nums[lastIndex], nums[minBiggerIndex] = nums[minBiggerIndex], nums[lastIndex]
-		i := lastIndex + 1
-		j := len(nums) - 1
-		for i < j {
-			nums[i], nums[j] = nums[j], nums[i]
-			i++
-			j--
-		}
+	}
+
+	i := lastIndex + 1
+	j := len(nums) - 1
+	for i < j {
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
 	}
 	return nums
 }
--- 31-next-permutation/next-permutation_test.go
+++ 31-next-permutation/next-permutation_test.go
@@ -12,6 +12,7 @@ type testcase struct {
 
 func Test(t *testing.T) {
 	cases := []testcase{
+		{[]int{3, 3, 2, 3, 4, 5, 6, 5, 4, 3}, []int{3, 3, 2, 3, 4, 6, 3, 4, 5, 5}},
 		{[]int{2, 3, 1, 3, 3}, []int{2, 3, 3, 1, 3}},
 		{[]int{2, 3, 1}, []int{3, 1, 2}},
 		{[]int{1, 3, 2}, []int{2, 1, 3}},
@@ -19,7 +20,6 @@ func Test(t *testing.T) {
 		{[]int{1, 1, 1}, []int{1, 1, 1}},
 		{[]int{1, 1, 2}, []int{1, 2, 1}},
 		{[]int{3, 3, 2, 3}, []int{3, 3, 3, 2}},
-		{[]int{3, 3, 2, 3, 4, 5, 6, 5, 4, 3}, []int{3, 3, 2, 3, 4, 6, 3, 4, 5, 5}},
 	}
 	for _, item := range cases {
 		output := nextPermutation(item.nums)
