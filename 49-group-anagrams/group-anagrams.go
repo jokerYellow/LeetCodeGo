882bf498835e69b88e0e8d9ad636c9c1865c05c4
piase <piase1910@163.com>
2019-12-26 11:38:49 +0800

    [add]:question of group anagrams

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -1,4 +1,32 @@
 package leetcode
+/*
+https://leetcode.com/problems/group-anagrams/
+49. Group Anagrams
+Medium
+
+2325
+
+138
+
+Add to List
+
+Share
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
+*/
 import "sort"
 
 type sortBytes []byte 
