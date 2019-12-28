f7314dcdd7749e7cd95160cb10fbefa7807ab2d2
JokerYellow <piase1910@163.com>
2019-12-28 09:09:10 +0800

    [refactor]:powx n

--- 50-powx-n/powx-n.go
+++ 50-powx-n/powx-n.go
@@ -1,10 +1,10 @@
 package powx_n
 
 func myPow(x float64, n int) float64 {
-	if x == 0 || x == 1 {
-		return x
+	if n == 0 {
+		return 1
 	}
-	if n == 1 {
+	if x == 0 || x == 1 || n == 1 {
 		return x
 	}
 	if n < 0 {
@@ -12,17 +12,9 @@ func myPow(x float64, n int) float64 {
 	}
 	var result float64 = x
 	i := 1
-	for {
-		newResult := result * result
-		if 2*i > n {
-			result = result * myPow(x, n-i)
-			break
-		} else if 2*i == n {
-			return newResult
-		} else {
-			result = newResult
-			i = 2 * i
-		}
+	for 2*i <= n {
+		result = result * result
+		i = 2 * i
 	}
-	return result
+	return result * myPow(x, n-i)
 }
